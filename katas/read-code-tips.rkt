8b874553886d360ffba5060082b557e1e9a79f64
Joshua Gibson <joshalexgibs@gmail.com>
2019-09-10 16:27:45 -0700

    minor touch-ups to read-code-tips.rkt

--- katas/read-code-tips.rkt
+++ katas/read-code-tips.rkt
@@ -9,13 +9,13 @@
 (define tips
   (list
    `001-easy
-   (read "Use awqrng-u8 as a function for a output a random number between 0-255.")
+   (read "Use awqrng-u8 as a function for an output a random number between 0-255.")
 
    `002-easy
-   (read "Use awqrng-u16 as a function for a output a random number between 0-65,535.")
+   (read "Use awqrng-u16 as a function for an output a random number between 0-65,535.")
 
    `003-easy
-   (read "Use awqrng-hex16 as a function for a output a random hexadecimal with a length of 4.")
+   (read "Use awqrng-hex16 as a function for an output a random hexadecimal with a length of 4.")
 
    `004-easy
    (read "You can change the amount of numbers lised in the function call by changing the number, which in the example is [9], to return up to 1024 random numbers between 0-255.")
@@ -24,6 +24,6 @@
    (read "You can change the amount of numbers lised in the function call by changing the number, which in the example is [10], to return up to 1024 random numbers between 0-65,535.")
 
    `006-easy
-   (read "In the hexadecimal you have 2 factors in the function that you can change. The amount of hexadecimals you recive, which in the example is [3], and the length of each hexadecimal, which in the example is [2].")
+   (read "In the hexadecimal you have 2 factors in the function that you can change: The amount of hexadecimals you receive, which in the example is [3], and the length of each hexadecimal, which in the example is [2].")
 
 ))
