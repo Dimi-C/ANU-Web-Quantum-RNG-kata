0be0422811e8d136d816f2c4f48ffb29875317d7
Dimitri Collas <dimitricollas@gmail.com>
2019-09-10 16:25:30 -0700

    Fixed tips

--- katas/read-code-tips.rkt
+++ katas/read-code-tips.rkt
@@ -18,12 +18,12 @@
    (read "Use awqrng-hex16 as a function for a output a random hexadecimal with a length of 4.")
 
    `004-easy
-   (read "You can change the amount of numbers lised in the function call by changing the number ex.[9] to return up to 1024 random numbers between 0-255.")
+   (read "You can change the amount of numbers lised in the function call by changing the number, which in the example is [9], to return up to 1024 random numbers between 0-255.")
 
    `005-easy
-   (read "You can change the amount of numbers lised in the function call by changing the number ex.[10] to return up to 1024 random numbers between 0-65,535.")
+   (read "You can change the amount of numbers lised in the function call by changing the number, which in the example is [10], to return up to 1024 random numbers between 0-65,535.")
 
    `006-easy
-   (read "In the hexadecimal you have 2 factors in the function that you can change. The amount of hexadecimals you recive which in the example is [3] and the length of each hexadecimal which in the example is [2].")
+   (read "In the hexadecimal you have 2 factors in the function that you can change. The amount of hexadecimals you recive, which in the example is [3], and the length of each hexadecimal, which in the example is [2].")
 
 ))
