454051a6eea43d45003a2260ddef32815c81b85d
Dimitri Collas <dimitricollas@gmail.com>
2019-09-10 16:21:12 -0700

    Added more tips

--- katas/read-code-tips.rkt
+++ katas/read-code-tips.rkt
@@ -9,21 +9,21 @@
 (define tips
   (list
    `001-easy
-   (read "Call it as a function.")
+   (read "Use awqrng-u8 as a function for a output a random number between 0-255.")
 
    `002-easy
-   (read "tip for number 2.")
+   (read "Use awqrng-u16 as a function for a output a random number between 0-65,535.")
 
    `003-easy
-   (read "tip for number 3.")
+   (read "Use awqrng-hex16 as a function for a output a random hexadecimal with a length of 4.")
 
    `004-easy
-   (read "tip for number 4.")
+   (read "You can change the amount of numbers lised in the function call by changing the number ex.[9] to return up to 1024 random numbers between 0-255.")
 
    `005-easy
-   (read "tip for number 5.")
+   (read "You can change the amount of numbers lised in the function call by changing the number ex.[10] to return up to 1024 random numbers between 0-65,535.")
 
    `006-easy
-   (read "tip for number 6.")
+   (read "In the hexadecimal you have 2 factors in the function that you can change. The amount of hexadecimals you recive which in the example is [3] and the length of each hexadecimal which in the example is [2].")
 
 ))
