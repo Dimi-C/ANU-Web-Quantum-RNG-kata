2769741cef3bf5daa86eaf2143600e1ba25e3771
Dimitri Collas <dimitricollas@gmail.com>
2019-09-10 15:44:26 -0700

    Added tips

--- katas/read-code-tips.rkt
+++ katas/read-code-tips.rkt
@@ -9,6 +9,21 @@
 (define tips
   (list
    `001-easy
-    (read "This is a tip for number 1 easy")
+   (read "Call it as a function.")
+
+   `002-easy
+   (read "tip for number 2.")
+
+   `003-easy
+   (read "tip for number 3.")
+
+   `004-easy
+   (read "tip for number 4.")
+
+   `005-easy
+   (read "tip for number 5.")
+
+   `006-easy
+   (read "tip for number 6.")
 
 ))
