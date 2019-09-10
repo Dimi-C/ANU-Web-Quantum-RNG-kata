7c354966f44ed40729f3afcebcfcc65042296225
Dimitri Collas <dimitricollas@gmail.com>
2019-09-10 14:41:37 -0700

    Added stimuli and tips into katas.rkt

--- katas.rkt
+++ katas.rkt
@@ -1,15 +1,25 @@
 #lang racket
 
-(provide katas)
+(provide katas
+         proto-katas)
 
 (require ts-kata-util/katas/main
-         "./katas/read-code-stimuli.rkt")
+         "./katas/read-code-stimuli.rkt"
+         "./katas/read-code-tips.rkt")
+
+(define proto-katas
+  (merge-collections (lang->kata-collection 'ANU-Web-Quantum-RNG-lang)
+                     code-values-all))
 
 (define katas
-  (apply fill-in-stimuli
-         (lang->kata-collection 'ANU-Web-Quantum-RNG-lang)
-         stimuli))
+  (apply fill-in-tips
+         (apply fill-in-stimuli proto-katas stimuli)
+         tips))
 
 (define-sub-collections katas
   easy
   )
+
+
+
+
