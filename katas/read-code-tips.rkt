88eea1a196840f5fdace5a4f1c6b6c7345a3e2d5
Dimitri Collas <dimitricollas@gmail.com>
2019-09-11 09:28:57 -0700

    added tips to rendering file

--- katas/read-code-tips.rkt
+++ katas/read-code-tips.rkt
@@ -3,25 +3,27 @@
 (provide tips)
 
 (require ts-kata-util/katas/main)
+(require ts-kata-util/katas/rendering/scribble)
+(require scribble/manual)
 
 (define tips
   (list
    `001-easy
-   (read "Use awqrng-u8 as a function for an output a random number between 0-255.")
+   (list "Use awqrng-u8 as a function for an output a random number between 0-255.")
 
    `002-easy
-   (read "Use awqrng-u16 as a function for an output a random number between 0-65,535.")
+   (list "Use awqrng-u16 as a function for an output a random number between 0-65,535.")
 
    `003-easy
-   (read "Use awqrng-hex16 as a function for an output a random hexadecimal with a length of 4.")
+   (list "Use awqrng-hex16 as a function for an output a random hexadecimal with a length of 4.")
 
    `004-easy
-   (read "You can change the amount of numbers lised in the function call by changing the number, which in the example is [9], to return up to 1024 random numbers between 0-255.")
+   (list "You can change the amount of numbers lised in the function call by changing the number, which in the example is [9], to return up to 1024 random numbers between 0-255.")
 
    `005-easy
-   (read "You can change the amount of numbers lised in the function call by changing the number, which in the example is [10], to return up to 1024 random numbers between 0-65,535.")
+   (list "You can change the amount of numbers lised in the function call by changing the number, which in the example is [10], to return up to 1024 random numbers between 0-65,535.")
 
    `006-easy
-   (read "In the hexadecimal you have 2 factors in the function that you can change: The amount of hexadecimals you receive, which in the example is [3], and the length of each hexadecimal, which in the example is [2].")
+   (list "In the hexadecimal you have 2 factors in the function that you can change: The amount of hexadecimals you receive, which in the example is [3], and the length of each hexadecimal, which in the example is [2].")
 
 ))
